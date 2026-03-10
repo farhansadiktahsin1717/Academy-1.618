2d9aea41b8a5cf6353f9138eb29847d76dcab215
farhansadiktahsin1717 <farhansadiktahsin04@gmail.com>
2026-03-10 19:35:30 +0600

    Fix API_BASE variable assignment in App.jsx

--- Academy 1.618-client/src/App.jsx
+++ Academy 1.618-client/src/App.jsx
@@ -6,6 +6,8 @@ import DashboardOverview from './pages/DashboardOverview'
 import Home from './pages/Home'
 import './App.css'
 
+const API_BASE = import.meta.env.VITE_API_BASE || 'http://localhost:8000'
+
 const API_BASE = import.meta.env.VITE_API_BASE || '/api/v1'
 
 const storage = {
