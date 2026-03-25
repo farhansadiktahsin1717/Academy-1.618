51c541a554424ff9c884d18c55d217cd94990787
farhansadiktahsin1717 <farhansadiktahsin04@gmail.com>
2026-03-25 22:37:54 +0600

    your message

--- Academy 1.618-client/public/sw.js
+++ Academy 1.618-client/public/sw.js
@@ -1,7 +1,8 @@
-const CACHE_NAME = 'academy-1618-shell-v1'
+const CACHE_NAME = 'academy-1618-shell-v2'
 const APP_SHELL = ['/', '/manifest.webmanifest', '/pwa-192.svg', '/pwa-512.svg']
 
 self.addEventListener('install', (event) => {
+  self.skipWaiting()
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)))
 })
 
@@ -11,6 +12,7 @@ self.addEventListener('activate', (event) => {
       Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))),
     ),
   )
+  self.clients.claim()
 })
 
 self.addEventListener('fetch', (event) => {
@@ -24,19 +26,22 @@ self.addEventListener('fetch', (event) => {
     return
   }
 
-  event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      if (cachedResponse) {
-        return cachedResponse
-      }
-
-      return fetch(event.request)
-        .then((networkResponse) => {
-          const responseClone = networkResponse.clone()
-          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, responseClone))
-          return networkResponse
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          const responseClone = response.clone()
+          caches.open(CACHE_NAME).then((cache) => cache.put('/', responseClone))
+          return response
         })
-        .catch(() => caches.match('/'))
-    }),
+        .catch(() => caches.match('/')),
+    )
+    return
+  }
+
+  event.respondWith(
+    fetch(event.request)
+      .then((networkResponse) => networkResponse)
+      .catch(() => caches.match(event.request)),
   )
 })
